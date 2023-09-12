093af1f943c3a33cdae56ee9d10fce30aabf458c
sanchaalii <sanchaalii333@gmail.com>
2023-09-13 01:26:20 +0530

    y first project

---
+++ src/test/java/Demo1/Test1.java
@@ -0,0 +1,17 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void method1() {
+		
+		System.out.println("HELLO1");
+	}
+		@Test
+		public void method2() {
+			
+			System.out.println("HELLO2");
+	}
+}
